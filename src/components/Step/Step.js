210ea167677d3d1f5a176bc191cb5c1c0354d6df
andrewyeh0128 <b0936486128@gmail.com>
2023-07-17 01:21:57 +0800

    alpha shop I

---
+++ src/components/Step/Step.js
@@ -0,0 +1,16 @@
+
+// import React, { Component } from 'react'
+import Step1 from "./Step1/Step1"
+import Step2 from "./Step2/Step2"
+
+function Step() {
+    return (
+      <div>
+        <Step1 />
+        <Step2 />
+        {/* <Step3 /> */}
+      </div>
+    )
+  }
+
+export default Step
---
+++ src/components/Step/StepProgress/StepProgress.js
@@ -0,0 +1,8 @@
+// import React from "react";
+// import styles from "./StepProgress.module.css";
+
+// export default function StepProgress() {
+//   return (
+
+//   )
+// }
