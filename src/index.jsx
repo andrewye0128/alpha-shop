dc951676871f6dc29b9aff66ca0f036a1a1dd438
andrewyeh0128 <b0936486128@gmail.com>
2023-08-07 15:45:39 +0800

    add ContextProvider

--- src/index.jsx
+++ src/index.jsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import  StepContextProvider  from './Context/StepContext.jsx'
+import { CartContextProvider } from './Context/CartContext';
+import { ShippnigContextProvider } from './Context/ShippingContext';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <StepContextProvider>
-      <App />
+      <ShippnigContextProvider>
+        <CartContextProvider>
+          <App />
+        </CartContextProvider>
+      </ShippnigContextProvider>
     </StepContextProvider>
   </React.StrictMode>
 );
